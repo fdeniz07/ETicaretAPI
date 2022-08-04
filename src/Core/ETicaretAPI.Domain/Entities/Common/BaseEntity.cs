6bde35cb73e2c681af97f5a1db400f4b13b9523f
Fatih Deniz <deniz.fatih@hotmail.de>
2022-08-04 23:56:29 +0200

    Recording of files with the Table Per Hierarchy approach

--- src/Core/ETicaretAPI.Domain/Entities/Common/BaseEntity.cs
+++ src/Core/ETicaretAPI.Domain/Entities/Common/BaseEntity.cs
@@ -6,7 +6,7 @@
 
         public DateTime CreatedDate { get; set; }
 
-        public DateTime UpdatedDate { get; set; }
+        virtual public DateTime UpdatedDate { get; set; }
 
     }
 }
---
+++ src/Core/ETicaretAPI.Domain/Entities/File.cs
@@ -0,0 +1,11 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ETicaretAPI.Domain.Entities
+{
+    public class File : BaseEntity
+    {
+        [NotMapped] // Ilgili base class daki propertynin migrate edilmemesini (base de virtual olarak isaretlenmeli) saglar
+        public override DateTime UpdatedDate { get => base.UpdatedDate; set => base.UpdatedDate = value; }
+    }
+}
